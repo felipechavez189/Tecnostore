6eba6952b969bf93f8ff5991fb1b753e604adebd
Felipe <chavezfelipe189@gmail.com>
2024-09-02 19:11:22 -0400

    Casi casi

--- src/app/pages/modificar/modificar.page.ts
+++ src/app/pages/modificar/modificar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modificar',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModificarPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private toastController: ToastController) { }
 
   ngOnInit() {
   }
 
+  async guardarCambios() {
+    // Muestra el toast
+    const toast = await this.toastController.create({
+      message: 'Modificado correctamente',
+      duration: 2000,
+      position: 'bottom'
+    });
+    await toast.present();
+
+    // Redirige a la página 'crud'
+    this.router.navigate(['/crud']);
+  }
 }

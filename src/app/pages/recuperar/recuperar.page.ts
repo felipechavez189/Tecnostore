fd36b4a84d2974f592409c3767ed3547d39ba706
Frvncoo69 <franco.miranda.2107@gmail.com>
2024-10-05 21:28:18 -0300

    formulario correo

--- src/app/pages/recuperar/recuperar.page.ts
+++ src/app/pages/recuperar/recuperar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recuperar',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recuperar.page.scss'],
 })
 export class RecuperarPage implements OnInit {
+  correo: string = ''; // Para almacenar el valor del input
 
-  constructor() { }
+  constructor(private alertController: AlertController, private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  // Método que se llama cuando se hace clic en el botón "Enviar Código"
+  async enviarCodigo() {
+    if (!this.correo) { // Verifica si el campo de correo está vacío
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Por favor, complete todos los campos.',
+        buttons: ['OK']
+      });
+      await alert.present();
+    } else if (!this.correo.includes('@')) { // Verifica si el campo de correo contiene un '@'
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Por favor, ingrese un correo electrónico válido que contenga "@"',
+        buttons: ['OK']
+      });
+      await alert.present();
+    } else {
+      // Redirige a la página de código si el campo no está vacío y contiene '@'
+      this.router.navigate(['/codigo']);
+    }
   }
-
 }

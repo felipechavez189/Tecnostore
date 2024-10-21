64d6a819a96763e4b08c9157cd37b7bf34bd8342
Frvncoo69 <franco.miranda.2107@gmail.com>
2024-10-21 17:43:58 -0300

    weas DE BUSQUEDA2

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';  // Importar CommonModule
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { HomePageRoutingModule } from './home-routing.module';  // Importar rutas de HomePage
+import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
 
 @NgModule({
   imports: [
-    CommonModule,  // Asegura la compatibilidad con directivas básicas de Angular
-    FormsModule,  // Para usar ngModel en el HTML
-    IonicModule,  // Componentes de Ionic
-    HomePageRoutingModule  // Enrutamiento específico de la página
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
   ],
   declarations: [HomePage]
 })
